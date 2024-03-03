03604a3e77afac8d63928d14c6728f917248e5f3
Nadhif <nadhifalrozin@gmail.com>
2024-03-03 10:01:36 +0700

    update

--- npm jawab_soal_1/soal_1.js
+++ npm jawab_soal_1/soal_1.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
+// GET Request
 axios.get('/user?ID=myid')
   .then(function (response_if_success) {
     console.log(response_if_success);
@@ -10,3 +11,15 @@ axios.get('/user?ID=myid')
   .finally(function () {
     console.log("Almays Executed");
   });
+
+// POST Request
+axios.post('/user', {
+  firstName: 'Nadhif',
+  lastName: 'Al Rozin'
+})
+.then(function (respon) {
+  console.log(respon);
+})
+.catch(function (error) {
+  console.log(error);
+});
