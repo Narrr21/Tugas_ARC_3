a61c13f448827ae622a60a3a19413d0ab17a3c54
Nadhif <nadhifalrozin@gmail.com>
2024-03-03 10:24:38 +0700

    adding input and list

---
+++ npm_jawab_soal_2/input.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+
+const nama = prompt('Masukkan nama: ');
+console.log(`Hallo, ${nama}!`);
---
+++ npm_jawab_soal_2/list.js
@@ -0,0 +1,5 @@
+let nilai = [80, 90, 85, 100, 95]
+
+console.log(nilai)
+console.log(nilai[0])
+console.log(nilai[4])
