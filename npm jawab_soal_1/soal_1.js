78a7710fad0cf07d678261b724d45c08fc21788e
Nadhif <nadhifalrozin@gmail.com>
2024-03-03 09:07:48 +0700

    Update soal 1.js

--- npm jawab_soal_1/soal_1.js
+++ npm jawab_soal_1/soal_1.js
@@ -1,1 +1,12 @@
-import axios from 'axios'
+const axios = require('axios');
+
+axios.get('/user?ID=myid')
+  .then(function (response_if_success) {
+    console.log(response_if_success);
+  })
+  .catch(function (response_if_error) {
+    console.log(response_if_error);
+  })
+  .finally(function () {
+    console.log("Almays Executed");
+  });
